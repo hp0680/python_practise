719b0f47f84b93d1a49fab33d5351c33caee8f6b
iceh <2223905355@qq.com>
2019-08-27 23:13:57 +0800

    Update 1LX.py

--- 1LX.py
+++ 1LX.py
@@ -1,7 +1,8 @@
 import turtle
-colors=['red','purple','blue','green','yellow','orange']
-t=turtle.Pen()
+colors = ['red','purple','blue','green','yellow','orange']
+t = turtle.Pen()
 turtle.bgcolor('black')
+
 for x in range(360):
     t.pencolor(colors[x%6])
     t.width(x/100+1)
